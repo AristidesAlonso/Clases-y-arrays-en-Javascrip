1239ccdc88b7fce0c007150c3f53a245ed404556
Aristides Alonso <aristidescastilla@hotmail.com>
2020-05-26 18:45:10 -0600

    dos script

--- paki.js
+++ paki.js
@@ -1,14 +1,19 @@
-class Pakiman
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+var coleccion = [];
+coleccion.push( new Pakiman("Cauchin", 100, 30) );
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
+
+for(var freddito of coleccion)
 {
-  constructor(n, v, a) //nombre, vida ataque
-  {
-    this.nombre = n;
-    this.vida = v;
-    this.ataque = a;
-  }
+  freddito.mostrar();
 }
 
-var cauchin = new Pakiman("cauchin", 100, 30);//100 de vida, ataque de 30
-var pokacho = new Pakiman("Pokacho", 80, 50);// 80 de vida, 50 de vida
-var tocinauro = new Pakiman("Tocinauro", 120, 40);//120 de vida atqu de 40
-console.log(cauchin, pokacho, tocinauro);
+for(var x in coleccion[0])
+{
+  console.log(x);
+}
